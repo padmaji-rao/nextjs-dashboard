c9255df05e828a4b5cfafb4edd5a98fd1b36281c
padmaji-rao <padmajiraokandulapati@gmail.com>
2025-07-17 21:47:01 +0530

    Fiest Commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,9 @@
+function Page() {
+  return (
+    <>
+      <p>This is Customers Page</p>
+    </>
+  );
+}
+
+export default Page;
---
+++ app/dashboard/page.tsx
@@ -0,0 +1,10 @@
+function Page()
+{
+    return(
+        <>
+        <h2>This is Dashboard Page</h2>
+        </>
+    )
+}
+
+export default Page;
