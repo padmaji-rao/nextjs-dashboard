a2f71d80b4a1d90a13970f3dd9e0a5d8ddd3f272
padmaji-rao <padmajiraokandulapati@gmail.com>
2025-07-18 20:04:31 +0530

    Added Streamig Concept for Fast Initial Load

--- app/ui/dashboard/nav-links.tsx
+++ app/ui/dashboard/nav-links.tsx
@@ -23,6 +23,7 @@ const links = [
 
 export default function NavLinks() {
   const pathname = usePathname();
+  // console.log(`path :---> ${pathname}`)
   return (
     <>
       {links.map((link) => {
